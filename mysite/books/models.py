4076c31f6ea16517cf7e150f1b802ccce0bbadf6
lakshmankashyap <lkashyap92@gmail.com>
2023-07-19 10:45:28 +0530

    Raise 404

--- mysite/books/models.py
+++ mysite/books/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 class Book(models.Model):
-    objects = None
+    def __str__(self):
+        return self.name + '-' + self.author
+
     name = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     price = models.CharField(max_length=100)
--- mysite/books/views.py
+++ mysite/books/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book
-from django.template import  loader
-
+from django.shortcuts import  render
+from django.http import Http404
 def index(request):
     all_books = Book.objects.all()
-    template = loader.get_template('books/index.html')
     context = {
         'all_books': all_books
     }
-    return HttpResponse(template.render(context,request))
+    return render(request,'books/index.html',context)
 
 
 def detail(request, book_id):
-    return HttpResponse(f"<h2>Details for Book ID: {book_id}</h2>")
+    try:
+        book = Book.objects.get(id=book_id)
+    except Book.DoesNotExist:
+        raise Http404('This book does not exist')
+    return render(request,'books/details.html',{'book':book})
